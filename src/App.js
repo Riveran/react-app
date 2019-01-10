1efdbf107223a60aaf63f4aceef02051c21453e3
Aleksey <riveranqq@gmail.com>
2019-01-10 18:10:02 +0300

    add PropTypes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import DayPicker from 'react-day-picker'
-import DayPickerInput from 'react-day-picker/DayPickerInput'
-import 'react-day-picker/lib/style.css'
 import ArticleList from './components/articleList'
 import articles from './components/fixtures'
 import './App.css'
 import UserForm from './components/user-form'
-import Selects from './components/filters/index'
+import Filters from './components/filters/index'
 
 class App extends React.Component {
-  state = {
-    openItem: ''
-  }
-
   render () {
     return (
       <div>
-        <Selects />
         <UserForm />
-        <DayPicker />
-        <DayPickerInput />
+        <Filters articles={articles} />
         <ArticleList articles={articles} />
       </div>
     )
